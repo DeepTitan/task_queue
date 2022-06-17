93573f701979cf3bc93009974374dbf2d3bd30a9
Isaiah Williams <isaiahwilliams@edgetheory.com>
2022-06-17 11:19:02 -0500

    add credentials

--- .history/user-data_20220617111044.sh
+++ .history/user-data_20220617111044.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-export AWS_ACCESS_KEY_ID="AKIAVY4XQ52Y44WENZ2S"
-export AWS_SECRET_ACCESS_KEY="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"
-export AWS_REGION="us-east-1"
+export AWS_ACCESS_KEY_ID=""
+export AWS_SECRET_ACCESS_KEY=""
+export AWS_REGION=""
 export SQS_URL=""
-yum update
-yum -y install python3 python3-pip git
+apt-get update -qq
+apt-get -qq -y install python3 python3-pip git
 curl -fsSL https://get.docker.com | bash -
 curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
